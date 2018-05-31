39d8709c387c9c44693b5c3446a1d5afa384ca2b
formynet <formynet@yahoo.com>
2018-05-30 20:55:17 -0400

    initial draft

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+//import {Observable} from 'rxjs/Observable';
+//import {map} from 'rxjs/operator/map';
+import 'rxjs/Rx';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
+
+  storeRecipes() {
+    const token = this.authService.getToken();
+    //https://angular-d8559.firebaseio.com/
+
+    return this.http.put('https://angular-d8559.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    const token = this.authService.getToken();
+
+    this.http.get('https://angular-d8559.firebaseio.com/recipes.json?auth=' + token)
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
+}
